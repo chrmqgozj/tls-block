dba92abe19c1dc722d7343389a54920e72a109a7
chrmqgozj <danmuginima@naver.com>
2025-06-09 18:42:04 +0900

    Add RST packet send in tls-block.cpp

--- tls-block.cpp
+++ tls-block.cpp
@@ -178,6 +178,138 @@ std::string handle_handshake_reassembly(const Key& key, const uint8_t* handshake
 	return "";
 }
 
+// checksum calculate
+unsigned short checksum(unsigned short *buffer, int size){
+	unsigned long cksum=0;
+	while(size >1) {
+		cksum+=*buffer++;
+		size -=sizeof(unsigned short);
+	}
+	if(size) {
+		cksum += *(unsigned char*)buffer;
+	}
+	cksum = (cksum >> 16) + (cksum & 0xffff);
+	cksum += (cksum >>16);
+	return (unsigned short)(~cksum);
+}
+
+// send packet to server (forward)
+void send_forward_packet(pcap_t* pcap, const u_char* org_packet, struct libnet_ipv4_hdr* iphdr, struct libnet_tcp_hdr* tcphdr, int data_len, uint8_t* my_mac) {
+	// length of each section
+	int ethdr_len, iphdr_len, tcphdr_len, packet_len;
+	ethdr_len = LIBNET_ETH_H;
+	iphdr_len = (iphdr -> ip_hl) * 4;
+	tcphdr_len = (tcphdr -> th_off) * 4;
+	packet_len = LIBNET_ETH_H + iphdr_len + tcphdr_len;
+
+	// packet to send
+	u_char packet[packet_len];
+	memset(packet, 0, packet_len);
+	memcpy(packet, org_packet, packet_len);
+
+	// ethernet header
+	struct libnet_ethernet_hdr* ethdr = (struct libnet_ethernet_hdr*)packet;
+	// change source mac
+	memcpy(ethdr->ether_shost, my_mac, ETHER_ADDR_LEN);
+
+	// ipv4 header
+	struct libnet_ipv4_hdr* new_iphdr = (struct libnet_ipv4_hdr*)(packet + ethdr_len);
+	new_iphdr->ip_len = htons(iphdr_len + tcphdr_len);
+	new_iphdr->ip_sum = 0;
+	new_iphdr->ip_sum = checksum((unsigned short*)new_iphdr, iphdr_len);
+
+	// tcp rst packet
+	struct libnet_tcp_hdr* new_tcphdr = (struct libnet_tcp_hdr*)(packet + ethdr_len + iphdr_len);
+	// change seq number, flag
+	new_tcphdr->th_seq = htonl(ntohl(tcphdr->th_seq) + data_len);
+	new_tcphdr->th_flags = TH_RST | TH_ACK;
+	new_tcphdr->th_sum = 0;
+
+	// calculate tcp checksum using pseudo header
+	u_char pseudo_hdr[12 + tcphdr_len];
+	memcpy(pseudo_hdr, &new_iphdr->ip_src.s_addr, 4);
+	memcpy(pseudo_hdr + 4, &new_iphdr->ip_dst.s_addr, 4);
+	pseudo_hdr[8] = 0;
+	pseudo_hdr[9] = IPPROTO_TCP;
+	unsigned short tcp_len = htons(tcphdr_len);
+	memcpy(pseudo_hdr + 10, &tcp_len, 2);
+	memcpy(pseudo_hdr + 12, new_tcphdr, tcphdr_len);
+
+	new_tcphdr->th_sum = checksum((unsigned short*)pseudo_hdr, 12 + tcphdr_len);
+
+	if (pcap_sendpacket(pcap, (const u_char*)packet, packet_len)) {
+		fprintf(stderr, "pcap_sendpacket error: %s\n", pcap_geterr(pcap));
+	}
+}
+
+// send packet to client (backward)
+void send_backward_packet(struct libnet_ipv4_hdr* iphdr, struct libnet_tcp_hdr* tcphdr, uint32_t data_len) {
+	// Fin은 직접 패킷 정보 입력해줘야 무한로딩 안 걸림. 정확한 이유는 모르겠지만 패킷의 어떤 정보가 소켓으로 전송하는 것과 안 맞는듯하다.
+	// length of each section
+	int iphdr_len, tcphdr_len, packet_len;
+	iphdr_len = (iphdr -> ip_hl) * 4;
+	tcphdr_len = (tcphdr -> th_off) * 4;
+	packet_len = iphdr_len + tcphdr_len;
+
+	// packet to send
+	u_char packet[packet_len];
+	memset(packet, 0, packet_len);
+
+	// exchange server <-> client
+	struct libnet_ipv4_hdr* new_iphdr = (struct libnet_ipv4_hdr*)packet;
+	// swap source and destination
+	new_iphdr->ip_src = iphdr->ip_dst;
+	new_iphdr->ip_dst = iphdr->ip_src;
+	new_iphdr->ip_hl = iphdr_len / 4;
+	new_iphdr->ip_v = 4;
+	new_iphdr->ip_len = htons(packet_len);
+	new_iphdr->ip_ttl = 128;
+	new_iphdr->ip_p = IPPROTO_TCP;
+	new_iphdr->ip_sum = 0;
+	new_iphdr->ip_sum = checksum((unsigned short*)new_iphdr, iphdr_len);
+
+	// exchange server <-> client / tcp fin packet
+	struct libnet_tcp_hdr* new_tcphdr = (struct libnet_tcp_hdr*)(packet + iphdr_len);
+	// swap source and destination
+	new_tcphdr->th_sport = tcphdr->th_dport;
+	new_tcphdr->th_dport = tcphdr->th_sport;
+	new_tcphdr->th_seq = tcphdr->th_ack;
+	new_tcphdr->th_ack = htonl(ntohl(tcphdr->th_seq) + data_len);
+	new_tcphdr->th_flags = TH_RST | TH_ACK;
+	new_tcphdr->th_off = tcphdr_len / 4;
+	// tcp header window size = 65535
+	new_tcphdr->th_win = htons(60000);
+	new_tcphdr->th_sum = 0;
+
+	// calculate tcp checksum using pseudo header
+	u_char pseudo_hdr[12 + tcphdr_len];
+	memcpy(pseudo_hdr, &new_iphdr->ip_src.s_addr, 4);
+	memcpy(pseudo_hdr + 4, &new_iphdr->ip_dst.s_addr, 4);
+	pseudo_hdr[8] = 0;
+	pseudo_hdr[9] = IPPROTO_TCP;
+	unsigned short tcp_len = htons(tcphdr_len);
+	memcpy(pseudo_hdr + 10, &tcp_len, 2);
+	memcpy(pseudo_hdr + 12, new_tcphdr, tcphdr_len);
+
+	new_tcphdr->th_sum = checksum((unsigned short*)pseudo_hdr, 12 + tcphdr_len);
+
+	// send through raw socket
+	int sd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	int on = 1;
+	// IP_HDRINCL: user provided ip header
+	setsockopt(sd, IPPROTO_IP, IP_HDRINCL, (char *)&on, sizeof(on));
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = new_tcphdr->th_dport;
+	address.sin_addr.s_addr = new_iphdr->ip_dst.s_addr;
+
+	if (sendto(sd, packet, packet_len, 0, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		fprintf(stderr, "Failed to send backward packet\n");
+	}
+	close(sd);
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 3) {
 		usage();
@@ -185,18 +317,34 @@ int main(int argc, char* argv[]) {
 	}
 
 	char* dev = argv[1];
-	char* target_server = argv[2];
-
+	char* pattern = argv[2];
 	char errbuf[PCAP_ERRBUF_SIZE];
-
 	pcap_t* pcap = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
 	if (pcap == NULL) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return -1;
 	}
 
-	printf("TLS Handshake Analysis started for server: %s\n", target_server);
+	uint8_t mac[6];
 
+	libnet_t* ln = libnet_init(LIBNET_LINK, dev, NULL);
+	if (ln == NULL) {
+		fprintf(stderr, "libnet_init failed\n");
+		libnet_destroy(ln);
+		return -1;
+	}
+
+	struct libnet_ether_addr* my_mac = libnet_get_hwaddr(ln);
+	if (my_mac == NULL) {
+		fprintf(stderr, "libnet_get_hwaddr failed\n");
+		libnet_destroy(ln);
+		return -1;
+	}
+
+	memcpy(mac, my_mac->ether_addr_octet, 6);
+	libnet_destroy(ln);
+
+	printf("TLS Handshake Analysis started for server: %s\n", pattern);
 	struct pcap_pkthdr* header;
 	const u_char* packet;
 
@@ -211,18 +359,20 @@ int main(int argc, char* argv[]) {
 		// Parse packet headers
 		if (header->len < 54) continue;
 
+		uint32_t ethdr_len, iphdr_len, tcphdr_len;
+
 		struct libnet_ethernet_hdr* ethdr = (struct libnet_ethernet_hdr*)packet;
 		if (ntohs(ethdr->ether_type) != ETHERTYPE_IP) continue;
+		ethdr_len = LIBNET_ETH_H;
 
-		struct libnet_ipv4_hdr* iphdr = (struct libnet_ipv4_hdr*)(packet + LIBNET_ETH_H);
+		struct libnet_ipv4_hdr* iphdr = (struct libnet_ipv4_hdr*)(packet + ethdr_len);
 		if (iphdr->ip_p != IPPROTO_TCP) continue;
+		iphdr_len = (iphdr->ip_hl) * 4;
 
-		uint32_t iphdr_len = (iphdr->ip_hl) * 4;
-		struct libnet_tcp_hdr* tcphdr = (struct libnet_tcp_hdr*)((uint8_t*)iphdr + iphdr_len);
-		uint32_t tcphdr_len = (tcphdr->th_off) * 4;
-
+		struct libnet_tcp_hdr* tcphdr = (struct libnet_tcp_hdr*)(packet + ethdr_len + iphdr_len);
 		// Check if it's HTTPS traffic (port 443)
 		if (ntohs(tcphdr->th_dport) != 443 && ntohs(tcphdr->th_sport) != 443) continue;
+		tcphdr_len = (tcphdr->th_off) * 4;
 
 		const uint8_t* data = packet + LIBNET_ETH_H + iphdr_len + tcphdr_len;
 		uint32_t data_len = ntohs(iphdr->ip_len) - iphdr_len - tcphdr_len;
@@ -253,8 +403,8 @@ int main(int argc, char* argv[]) {
 		if (!sni.empty()) {
 			const char* server_name = sni.c_str();
 			printf("Found SNI: %s\n", server_name);
-			
-			if (memmem(server_name, strlen(server_name), target_server, strlen(target_server)) != NULL) {
+
+			if (memmem(server_name, strlen(server_name), pattern, strlen(pattern)) != NULL) {
 				printf("*** TARGET SERVER DETECTED: %s ***\n", sni.c_str());
 
 				char src_ip[INET_ADDRSTRLEN], dst_ip[INET_ADDRSTRLEN];
@@ -264,10 +414,12 @@ int main(int argc, char* argv[]) {
 				printf("Connection: %s:%d -> %s:%d\n", 
 						src_ip, ntohs(tcphdr->th_sport),
 						dst_ip, ntohs(tcphdr->th_dport));
-				
-				
+
+
 				// Here you would send blocking packets in a real implementation
 				printf("Would block this connection\n");
+				send_backward_packet(iphdr, tcphdr, data_len);
+				send_forward_packet(pcap, packet, iphdr, tcphdr, data_len, mac);
 			}
 		}
 	}
